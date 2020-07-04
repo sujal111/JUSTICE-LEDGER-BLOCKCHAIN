efc2945c9c55d2b1a294608027fffa9649ecfbed
sujal111 <62564807+sujal111@users.noreply.github.com>
2020-07-03 21:32:35 -0700

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    develop: {
+      port: 8545
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://kovan.infura.io/v3/d6567ea751014dc6b2426e02cd9ac714"
+        );
+      },
+      network_id: 42,
+      gas: 4500000,
+      gasPrice: 10000000000
+    },
+    matic: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://testnet2.matic.network"
+        );
+      },
+      network_id: 8995,
+      gas: 4500000,
+      gasPrice: 10000000000
+    }
+  }
+};
